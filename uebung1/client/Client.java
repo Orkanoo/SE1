d3b793f447658adc276f83601999addde337c318
Orkanoo <tbeilschmidt@t-online.de>
2026-04-22 23:02:16 +0200

    start
    Anfang von Aufgabe 1

---
+++ uebung1/client/Client.java
@@ -0,0 +1,9 @@
+package uebung1.client;
+
+public class Client {
+    public void printTransformation(int number) {
+        String result = ""; // Hier fehlt noch was
+        System.out.println("Die römische Schreibweise der Zahl " +
+        number + " ist: " + result);
+    }
+}
